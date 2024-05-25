e3ed76622ae8f1197df60c13097682aa2119ca23
siddharth2016 <siddharthchandragzb@gmail.com>
2024-05-25 20:01:50 +0530

    learning access modifiers

---
+++ Scaler/LowLevelDesign/src/lld1/accessmodifiers/package2/Test.java
@@ -0,0 +1,9 @@
+package src.lld1.accessmodifiers.package2;
+
+public class Test {
+    public static void main(String[] args) {
+        AnotherStudent anotherStudent = new AnotherStudent();
+        System.out.println(anotherStudent.name);
+//        System.out.println(anotherStudent.marks);
+    }
+}

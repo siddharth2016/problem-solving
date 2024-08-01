4ac7c1d32e335637de36dbc1b38ce40d03f411d1
siddharth2016 <siddharthchandragzb@gmail.com>
2024-08-01 17:35:27 +0530

    learning inheritance

---
+++ Scaler/LowLevelDesign/src/lld1/exercises/exec1.java
@@ -0,0 +1,25 @@
+package src.lld1.exercises;
+
+import java.util.Stack;
+
+public class exec1 {
+
+    public static void main(String[] args) {
+        Stack<Integer> st = new Stack<>();
+        st.push(10);
+        st.push(20);
+        st.push(30);
+
+        // Make a call to display method as per question requirements here
+        display(st);
+
+    }
+
+    // Write the display method as per requirements of question here
+    public static void display(Iterable<?> list) {
+        for(Object val: list) {
+            System.out.println(val);
+        }
+    }
+
+}

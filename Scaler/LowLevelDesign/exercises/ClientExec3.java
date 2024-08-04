907aea85185dd4cad21a99d8e1918fbeecc0b44e
siddharth2016 <siddharthchandragzb@gmail.com>
2024-08-04 14:56:19 +0530

    learning threads

---
+++ Scaler/LowLevelDesign/exercises/ClientExec3.java
@@ -0,0 +1,37 @@
+package exercises;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.concurrent.*;
+
+public class ClientExec3 {
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        ArrayCreator arrayCreator = new ArrayCreator(n);
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        Future<List<Integer>> futureArrayCreator = executorService.submit(arrayCreator);
+        System.out.println(futureArrayCreator.get());
+    }
+
+}
+
+class ArrayCreator implements Callable<List<Integer>> {
+
+    private int n;
+
+    public ArrayCreator(int n) {
+        this.n = n;
+    }
+
+    @Override
+    public List<Integer> call() throws Exception {
+        List<Integer> array = new ArrayList<>();
+        for(int i=0; i<n; i++) {
+            array.add(i+1);
+        }
+        return array;
+    }
+}

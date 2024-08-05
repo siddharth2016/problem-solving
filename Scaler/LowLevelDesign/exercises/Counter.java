27878f7e22eeb236d63f532cefe5152814c73158
siddharth2016 <siddharthchandragzb@gmail.com>
2024-08-05 22:28:41 +0530

    learning threads

---
+++ Scaler/LowLevelDesign/exercises/Counter.java
@@ -0,0 +1,22 @@
+package exercises;
+
+public class Counter {
+
+    private int count;
+
+    public Counter(int count) {
+        this.count = count;
+    }
+
+    public synchronized void incValue(int offset) {
+        count += offset;
+    }
+
+    public synchronized int getValue() {
+        return count;
+    }
+
+    public synchronized void decValue(int offset) {
+        count -= offset;
+    }
+}

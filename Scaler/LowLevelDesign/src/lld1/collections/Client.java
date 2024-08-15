e0d5172a008aee33d4a7bd821229e32c4e97b9eb
siddharth2016 <siddharthchandragzb@gmail.com>
2024-08-15 21:45:16 +0530

    learning collections

---
+++ Scaler/LowLevelDesign/src/lld1/collections/Client.java
@@ -0,0 +1,26 @@
+package src.lld1.collections;
+
+import java.util.*;
+
+public class Client {
+
+    public static void main(String[] args) {
+        Set<Integer> integerHashSet = new HashSet<>();
+        Set<Integer> integerTreeSet = new TreeSet<>();
+        SortedSet<Integer> sortedSet = new TreeSet<>();
+        Set<Integer> linkSet = new LinkedHashSet<>();
+
+        Queue<Integer> integerQueue = new LinkedList<>();
+        Deque<Integer> integerDeque = new LinkedList<>();
+
+        List<Integer> integerList = new ArrayList<>();
+        List<Integer> integerList1 = new LinkedList<>();
+        List<Integer> integerList2 = new Stack<>();
+        Stack<Integer> integerStack = new Stack<>();
+
+        Map<Integer, Integer> map1 = new HashMap<>();
+        Map<Integer, Integer> map2 = new TreeMap<>();
+        Map<Integer, Integer> map3 = new LinkedHashMap<>();
+    }
+
+}

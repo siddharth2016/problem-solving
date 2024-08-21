20cf8c2d43523957cef6e523155229ca48acefda
siddharth2016 <siddharthchandragzb@gmail.com>
2024-08-21 17:08:43 +0530

    learning singleton

---
+++ Scaler/LowLevelDesign/src/lld2/singleton/logger/Logger.java
@@ -0,0 +1,20 @@
+package src.lld2.singleton.logger;
+
+import org.springframework.boot.logging.LogLevel;
+
+import java.io.FileNotFoundException;
+
+public interface Logger {
+
+    void log(LogLevel level, String message);
+
+    void setLogFile(String filePath) throws FileNotFoundException;
+
+    String getLogFile();
+
+    // Flush the log entries to the file
+    void flush();
+
+    // Close the logger and release any resources
+    void close();
+}
